345b42461b6f4fa86705b7f40a730194b1ed7963
Death-by-pip <innerprotext@gmail.com>
2024-03-06 21:40:34 -0600

    finished project

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ enable_display_window = True
 
 display_size = 6
 block_size = 50
-tick = 1
+tick = 20
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -51,13 +51,14 @@ class Player(pygame.sprite.Sprite):
         self.deltay = 0
         self.x = 0
         self.y = 0
+        self.dead = False
 
     # def redefine_player(self,x,y):
     #     self.image = pygame.Surface((self.size*2, self.size*2), pygame.SRCALPHA)
     #     pygame.draw.rect(self.image, (160, 210, 80, 250), pygame.rect.Rect(0,0,self.size*2,self.size*2))
     #     self.rect = self.image.get_rect(center = (0,0))
 
-    def move(self, map_, moves, xspeed=2, jumpspeed=3, acceleration=-1.8, maxspeed=5):  # moves = [left, right, up]
+    def move(self, map_, moves, xspeed=4, jumpspeed=-10, acceleration=1, maxspeed=10):  # moves = [left, right, up]
         # keys = pygame.key.get_pressed()
         # print("")
         multiplier = block_size/tick
@@ -69,7 +70,7 @@ class Player(pygame.sprite.Sprite):
             self.deltax -= xspeed*multiplier
         if moves[2]:
             jump = True
-        self.y-=1
+        self.y+=1
         if "#" in self.get_collided(map_):
             if jump:
                 self.deltay = jumpspeed*multiplier
@@ -77,13 +78,13 @@ class Player(pygame.sprite.Sprite):
                 self.deltay = 0
         else:
             self.deltay += acceleration*multiplier
-        self.y+=1
-        if self.deltay>maxspeed*block_size:
-            self.deltay = maxspeed*block_size
-        elif self.deltay < -maxspeed*block_size:
-            self.deltay = -maxspeed*block_size
-        deltax = self.deltax
-        deltay = self.deltay
+        self.y-=1
+        if self.deltay>maxspeed*multiplier:
+            self.deltay = maxspeed*multiplier
+        elif self.deltay < -maxspeed*multiplier:
+            self.deltay = -maxspeed*multiplier
+        deltax = round(self.deltax)
+        deltay = round(self.deltay)
         while (deltax!=0 or deltay!=0):
             if deltax<0:
                 self.x -= 1
@@ -95,7 +96,7 @@ class Player(pygame.sprite.Sprite):
                 elif deltax>0:
                     self.x -= 1
                 deltax = 0
-            if deltax<0:
+            elif deltax<0:
                 deltax += 1
             elif deltax>0:
                 deltax -= 1
@@ -110,7 +111,7 @@ class Player(pygame.sprite.Sprite):
                 elif deltay>0:
                     self.y -= 1
                 deltay = 0
-            if deltay<0:
+            elif deltay<0:
                 deltay += 1
             elif deltay>0:
                 deltay -= 1
@@ -122,8 +123,12 @@ class Player(pygame.sprite.Sprite):
     def get_collided(self, map_):
         self.rect.center = (self.x, self.y)
         collided = []
-        y = [(self.rect.top)//block_size, self.y//block_size, (self.rect.bottom-1)//block_size]
-        x = [(self.rect.left)//block_size, self.x//block_size, (self.rect.right-1)//block_size]
+        y = [(self.rect.top)//block_size, self.rect.centery//block_size, (self.rect.bottom-1)//block_size]
+        x = [(self.rect.left)//block_size, self.rect.centerx//block_size, (self.rect.right-1)//block_size]
+        # lvl = [i for i in map_]
+        # lvl[y[1]] = lvl[y[1]][:x[1]]+"$"+lvl[y[1]][x[1]+1:]
+        # print("\n".join(lvl))
+        # print(x, y)
         if x[2]==x[1]:
             x.pop(2)
             # print("left")
@@ -144,15 +149,28 @@ class Player(pygame.sprite.Sprite):
         #     collide2 = "down"
         # if len(y)==1:
         #     collide2 = ""
+        # print(x, y)
+        global playing
         if y[-1]>len(map_):
             print("Player has died.")
+            # global playing
+            self.dead = True
             return ["X"]
         for x_ in x:
             for y_ in y:
                 try:
                     collided.append(map_[y_][x_])
                 except:
-                    collided.append("#")
+                    collided.append("X")
+        if "@" in collided:
+            global advance
+            advance = True
+        if ">" in collided:
+            global win
+            # global playing
+            playing = False
+            win = True
+        print(collided)
         return collided
 
     def update(self):
@@ -161,26 +179,32 @@ class Player(pygame.sprite.Sprite):
         if enable_display_window:
             window.fill((0,0,0,255))
             # print(self.x, self.y)
-            window_rect.center = (self.x, screen.get_width()-self.y)
+            window_rect.center = (self.x, self.y)
             # print(window_rect.center)
             # # print(window_rect.centerx)
             # print(window_rect.width/2)
-            print(screen.get_width())
-            if self.x < window_rect.width/2:
-                print("left")
+            # print(screen.get_width())
+            height = 0
+            width = 0
+            if self.x < window_rect.width//2:
+                # print("left")
+                width = window_rect.width//2 - window_rect.centerx
                 window_rect.centerx = window_rect.width/2
-            elif (self.x+window_rect.width/2)>screen.get_width():
-                print("right")
+            elif (self.x+window_rect.width//2)>screen.get_width():
+                # print("right")
+                width = window_rect.width//2 - window_rect.centerx
                 window_rect.centerx = screen.get_width()-window_rect.width/2
-            if screen.get_width()-self.y < window_rect.height/2:
-                print("up")
+            if self.y < window_rect.height//2:
+                # print("up")
+                height = window_rect.height//2 - window_rect.centery
                 window_rect.centery = window_rect.height/2
-            elif (screen.get_width()-self.y+window_rect.height/2)>screen.get_height():
-                print("down")
+            elif (self.y+window_rect.height//2)>screen.get_height():
+                # print("down")
+                height = window_rect.height//2 - window_rect.centery + block_size
                 window_rect.centery = screen.get_height()-window_rect.height/2
-            print(window_rect.center)
+            # print(window_rect.center)
             window.blit(screen.subsurface(window_rect).copy(), (0, 0))
-            window.blit(self.image, (window_rect.width/2, window_rect.height/2))
+            window.blit(self.image, (window_rect.width//2-width-self.size//2, window_rect.height//2-height-self.size//2))
 
 def clamp(value, min, max):
     if value<min:
@@ -205,7 +229,7 @@ def load_level(stage, sleep=False):
     lvl1 = lvl[:lvl.index("zzz")]
     lvl2 = lvl[lvl.index("zzz")+1:]
     if sleep:
-        lvl = [(lvl1[i].replace("$"," ").replace("@",":").replace(">"," ")+lvl2[i]).replace("[","").replace("]","") for i in range(len(lvl1))]
+        lvl = [(lvl1[i].replace("$"," ").replace("@",":").replace(">"," ").replace("[","").replace("]","")+lvl2[i]).replace("[","").replace("]","") for i in range(len(lvl1))]
     else:
         lvl = [row.replace("[","").replace("]","") for row in lvl1]
     for n, row in enumerate(lvl):
@@ -246,16 +270,27 @@ def load_level(stage, sleep=False):
     if enable_display_window:
         window_rect.center = (player.x+block_size, player.y-block_size)
         window.blit(screen, (0,0), window_rect)
+    print("\n".join(lvl))
     return lvl
 
 stages = ["intro"]
 player = Player()
+advance = False
 
 # while True:
+win = False
 loaded_stage = load_level(stages[0])
 playing = True
 moves = [False, False, False]
+current = (stages[0], False)
 while playing:
+    if advance:
+        current = (stages[0], True)
+        loaded_stage = load_level(stages[0], True)
+        advance = False
+    if player.dead:
+        player.dead = False
+        loaded_stage = load_level(current[0], current[1])
     # keys = pygame.key.get_pressed()
     # if keys[pygame.K_ESCAPE]:
         # pygame.quit()
@@ -340,4 +375,20 @@ while playing:
     pygame.display.flip()
 
     clock.tick(tick)
+if win:
+    window.fill((0,0,0,255))
+    pygame.font.init()
+    font = pygame.font.SysFont('Courier New', 30)
+    text_surface = font.render('YOU WIN', False, (0, 255, 0, 255))
+    window.blit(text_surface, (window_rect.width//2, window_rect.height//2))
+    pygame.display.flip()
+    while not playing:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                # pygame.quit()
+                playing = True
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_ESCAPE:
+                    # pygame.quit()
+                    playing = True
 pygame.quit()
